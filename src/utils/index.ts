3005995ca645fdda238f14ab8a8e0e41fb649ad5
Hazo Baikulov <spacehaz@gmail.com>
2026-01-16 13:59:12 +0300

    removed utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,25 +1,13 @@
-import toHex from './to-hex'
 import alertError from './alert-error'
-import defineNetworkName from './define-network-name'
 import defineExplorerURL from "./define-explorer-url"
 import shortenString from './shorten-string'
-import defineNetworkIcon from './define-network-icon'
 import generateMetadataUtil from './generate-metadata'
-import metadataUrlResolve from './metadata-resolve-url'
 import createQueryString from './create-query-string'
-import defineEthersSigner from './define-ethers-signer'
 import copyToClipboard from './copy-to-clipboard'
 import formatDate from './format-date'
 import formatTime from './format-time'
-import checkApproveTransaction from './check-approve-transaction'
-import formatTokensAmount from './format-tokens-amount'
 import formatExpiration from './format-expiration'
 import api from './api'
-import checkTransactionReceipt from './check-transaction-receipt'
-import defineIfBrowserIsValid from './define-if-browser-is-valid'
-import defineIfBrowserIsMises from './define-if-browser-is-mises'
-import defineIfKeyHasAlreadyBeenCreated from './define-if-user-key-has-already-been-created'
-
 import defineGroup from './define-group';
 import createSemaphoreIdentity from './create-semaphore-identity';
 import defineApiUrl from './define-api-url';
@@ -30,26 +18,19 @@ import defineZuploNetworkName from './define-zuplo-network-name';
 import defineRelatedVerification from './define-related-verification';
 import calculateAvailablePoints from './calculate-available-points';
 import msToTime from './ms-to-time';
-import areArraysEqual from './are-arrays-equal';
-import areObjectsEqual from './are-objects-equal';
 import defineInitialSelectedVerifications from './define-initial-selected-verifications';
 import getOAuthSemaphoreData from './get-oauth-semaphore-data'
 import defineGroupByZKTLSResult from './define-group-by-zktls-result'
 import getZKTLSSemaphoreData from './get-zk-tls-semaphore-data'
 
-
 export {
   defineGroup,
   createSemaphoreIdentity,
-
-  areArraysEqual,
-  areObjectsEqual,
   msToTime,
   defineApiUrl,
   defineTaskPointsRange,
   defineTaskByCredentialGroupId,
   calculateScope,
-
   defineZuploNetworkName,
   defineRelatedVerification,
   calculateAvailablePoints,
@@ -58,24 +39,13 @@ export {
   defineGroupByZKTLSResult,
   getZKTLSSemaphoreData,
   alertError,
-  formatTokensAmount,
   formatDate,
   formatTime,
   copyToClipboard,
-  defineEthersSigner,
-  metadataUrlResolve,
   createQueryString,
   generateMetadataUtil,
-  defineNetworkIcon,
-  defineIfKeyHasAlreadyBeenCreated,
   shortenString,
-  defineNetworkName,
   defineExplorerURL,
-  checkApproveTransaction,
-  toHex,
   formatExpiration,
-  api,
-  checkTransactionReceipt,
-  defineIfBrowserIsValid,
-  defineIfBrowserIsMises
+  api
 }
--- src/utils/to-hex.tsx
+++
@@ -1,2 +0,0 @@
-const toHex = (d: string | number) => ('0x' + Number(d).toString(16))
-export default toHex
