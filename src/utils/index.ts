60ab317c5ead8d1cda0c96e8a56c19c391763284
Hazo Baikulov <spacehaz@gmail.com>
2026-03-08 18:50:11 +0300

    updates for widget

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -30,8 +30,6 @@ import isValidAuthSuccessPayload from './is-valid-auth-success-payload'
 import defineTaskIcon from './define-task-icon'
 import generateRequestId from './generate-request-id'
 import { getAppSemaphoreGroupId, getAllScores } from './registry-contract'
-import isFarcasterApp from './is-farcaster-app'
-
 
 export {
     isValidZKTLSErrorMessage,
@@ -67,5 +65,4 @@ export {
   defineGroupForAuth,
   getAppSemaphoreGroupId,
   getAllScores,
-  isFarcasterApp,
 }
